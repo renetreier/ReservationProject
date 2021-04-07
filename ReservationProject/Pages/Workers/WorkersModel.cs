7f1c8917a18b0e1967f6c42b71ca516f206596f4
Kalle Pilli <kapill@ttu.ee>
2021-04-07 12:00:39 +0300

    Worker CRUD ja WorkersModel töötab

---
+++ ReservationProject/Pages/Workers/WorkersModel.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReservationProject.Data;
+using ReservationProject.Infra;
+
+namespace ReservationProject.Soft.Pages.Workers
+{
+    public class WorkersModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public WorkersModel(ApplicationDbContext context) => _context = context;
+
+        public IActionResult OnGetCreate()
+        {
+            return Page();
+        }
+        [BindProperty] public Worker Worker { get; set; }
+
+        public async Task<IActionResult> OnPostCreateAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Workers.Add(Worker);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+
+        public async Task<IActionResult> OnGetEditAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Worker = await _context.Workers.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Worker == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostEditAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Worker).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!WorkerExists(Worker.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool WorkerExists(int id)
+        {
+            return _context.Workers.Any(e => e.Id == id);
+        }
+
+        public async Task<IActionResult> OnGetDeleteAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Worker = await _context.Workers.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Worker == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostDeleteAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Worker = await _context.Workers.FindAsync(id);
+
+            if (Worker != null)
+            {
+                _context.Workers.Remove(Worker);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        public async Task<IActionResult> OnGetDetailsAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Worker = await _context.Workers.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Worker == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public IList<Worker> WorkerList { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            WorkerList = await _context.Workers.ToListAsync();
+        }
+    }
+}
