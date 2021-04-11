23dc4cce5e5a031d157b49700832c34e21110d38
renetreier <78070145+renetreier@users.noreply.github.com>
2021-04-11 21:31:27 +0300

    Kõik indexid kasutavad korrektselt _index layouti, tõin extensionsisse showindextabel ja panin indexid seda kasutama

--- Pages/ReservationsModel.cs
+++ Pages/ReservationsModel.cs
@@ -1,14 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ReservationProject.Data;
 using ReservationProject.Infra;
-using ReservationProject.Pages;
 
 namespace ReservationProject.Pages
 {
--- Pages/RoomsModel.cs
+++ Pages/RoomsModel.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using ReservationProject.Data;
 using ReservationProject.Infra;
-using ReservationProject.Pages;
 //TODO Vaja puhastada ja refaktoorida
 namespace ReservationProject.Pages
 {
