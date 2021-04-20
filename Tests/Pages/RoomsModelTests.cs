74b75868faa28044c711b41104df08db256e8e01
Kalle Pilli <kapill@ttu.ee>
2021-04-20 14:22:29 +0300

    Testid töötavad

--- Tests/Pages/RoomsModelTests.cs
+++ Tests/Pages/RoomsModelTests.cs
@@ -25,66 +25,6 @@ namespace ReservationProject.Tests.Pages
             mockRepo = new TestRoomsRepo();
             pageModel = new RoomsModel((IRoomsRepo)mockRepo);
         }
-        
-        //public async Task<IActionResult> OnGetDeleteAsync(string id = "")
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    Room = await db.Rooms.FirstOrDefaultAsync(m => m.RoomId == id);
-
-        //    if (Room == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    return Page();
-        //}
-
-
-
-
-
-
-        //    [TestMethod]
-        //    public void OnGetDeleteAsyncTestPageResult()
-        //        => Assert.IsInstanceOfType(
-        //            onGetDeleteAsync(GetRandom.Int32(), new TData()), typeof(PageResult));
-
-        //    [TestMethod]
-        //    public void OnGetDetailsAsyncTestItemNotFound()
-        //        => Assert.IsInstanceOfType(onGetDetailsAsync(GetRandom.Int32()), typeof(NotFoundResult));
-        //    [TestMethod]
-        //    public void OnGetDetailsAsyncTestIsCallingGet()
-        //    {
-        //        var i = GetRandom.Int32();
-        //        onGetDetailsAsync(i);
-        //        Assert.AreEqual($"Get {i}", mockRepo.Actions[0]);
-        //    }
-        //    [TestMethod]
-        //    public void OnGetDetailsAsyncTestPageResult()
-        //        => Assert.IsInstanceOfType(
-        //            onGetDetailsAsync(GetRandom.Int32(), new TData()), typeof(PageResult));
-
-        //    [TestMethod]
-        //    public void OnGetEditAsyncTestItemNotFound()
-        //        => Assert.IsInstanceOfType(onGetEditAsync(GetRandom.Int32()), typeof(NotFoundResult));
-        //    [TestMethod]
-        //    public void OnGetEditAsyncTestIsCallingGet()
-        //    {
-        //        var i = GetRandom.Int32();
-        //        onGetEditAsync(i);
-        //        Assert.AreEqual($"Get {i}", mockRepo.Actions[0]);
-        //    }
-        //    [TestMethod]
-        //    public void OnGetEditAsyncTestPageResult()
-        //        => Assert.IsInstanceOfType(
-        //            onGetEditAsync(GetRandom.Int32(), new TData()), typeof(PageResult));
-
-        //    [TestMethod]
-        //    public void OnGetCreateTestPageResult()
-        //        => Assert.IsInstanceOfType(pageModel.OnGetCreate(), typeof(PageResult));
     }
 }
 
