9782ecb014803d7bcb341ca3f3d0a8e3d3dc0f9f
renetreier <78070145+renetreier@users.noreply.github.com>
2021-04-24 13:37:26 +0300

    parandasin Roomavailable ja panin katkised testid tööle, lisaks panin indexites vaikimise sordi peale

--- Infra/RoomsRepo.cs
+++ Infra/RoomsRepo.cs
@@ -1,4 +1,8 @@
 ﻿
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using ReservationProject.Data;
 using ReservationProject.Domain;
 using ReservationProject.Infra.Common;
@@ -9,6 +13,10 @@ namespace ReservationProject.Infra {
     public sealed class RoomsRepo : BaseRepo<Room>, IRoomsRepo
     {
         public RoomsRepo(ApplicationDbContext c) : base(c, c?.Rooms) { }
+        public override async Task<List<Room>> Get()
+        {
+            return await Set.AsNoTracking().OrderBy(r => r.RoomName).ToListAsync();
+        }
     }
 }
 
